2d291793684df63a39b39564c60c52569fb91875
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-05 23:11:31 -0500

    Step2: Write out tests

--- section_2/#1_twoSum.js
+++ section_2/#1_twoSum.js
@@ -10,3 +10,11 @@
 // Will there always be a solution available?
 // What do we return if there is no solution?
 // Can there be multiple pairs that add up to the target?
+
+
+// Step 2: Write out some test cases
+// [1,3,7,9,2], t = 11, return [3,4]
+// [1,3,7,9,2], t = 25, return null
+// [],          t = 1,  return null
+// [5],         t = 5,  return null
+// [1,6],       t = 7,  return [0,1]
