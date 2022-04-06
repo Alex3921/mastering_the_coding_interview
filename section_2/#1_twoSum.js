b8bd699d2ed3c26d93e2dc98e1b0df0e9d086396
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-05 23:10:51 -0500

    Step1: Verify constraints

--- section_2/#1_twoSum.js
+++ section_2/#1_twoSum.js
@@ -2,3 +2,11 @@
 // return the indices of the two 
 // numbers that add up to a given target.
 
+
+// Step 1: Verify the constrains
+
+// Are all the numbers positive or can there be negatives?
+// Are there duplicate numbers in the array?
+// Will there always be a solution available?
+// What do we return if there is no solution?
+// Can there be multiple pairs that add up to the target?
