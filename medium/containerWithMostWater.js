351ce4158a917280925207ad9632392172335238
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-06 21:57:55 -0500

    Step 2: Coming up with test cases

--- medium/containerWithMostWater.js
+++ medium/containerWithMostWater.js
@@ -4,10 +4,17 @@
 // hold the greatest amount of water. Return the are of water it would hold.
 
 
-// Step 1: Verify the constrains.
+// Step 1: Verify the constrains
 // Q: Does the tickness of the lines affect the area?
 // A: No, assume they take up no space.
 // Q: Do the left and right sides of the graph count as walls?
 // A: No, the sides cannot be used to form a container.
 // Q: Does a higher line inside our container affect our area?
 // A: No, lines inside a container don't affect the area.
+
+
+// Step 2: Coming up with test cases
+// []           => return 0
+// [8]          => return 0
+// [6,9,3,4,5,8]=> return [1,5]
+
