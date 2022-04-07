99a83bb484baa1bf11cf981105dee645053c9e02
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-06 21:37:53 -0500

    Check constraints

--- medium/containerWithMostWater.js
+++ medium/containerWithMostWater.js
@@ -4,3 +4,10 @@
 // hold the greatest amount of water. Return the are of water it would hold.
 
 
+// Step 1: Verify the constrains.
+// Q: Does the tickness of the lines affect the area?
+// A: No, assume they take up no space.
+// Q: Do the left and right sides of the graph count as walls?
+// A: No, the sides cannot be used to form a container.
+// Q: Does a higher line inside our container affect our area?
+// A: No, lines inside a container don't affect the area.
