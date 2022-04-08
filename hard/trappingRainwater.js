494cecc3f30865e47d8837b4d7a6928223b78e4c
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-07 22:47:34 -0500

    Step 1: Verify the constraints

---
+++ hard/trappingRainwater.js
@@ -0,0 +1,11 @@
+// Statement: Given ana array of integers representing an elevation 
+// map where the width of each bar is 1, return how much rainwater can be trapped.
+
+
+// Step 1: Verify the constraints
+// Q: Do the left and right sides of the graph count as walls?
+// A: No, the sides are not walls.
+// Q: Will there be negative integers?
+// A: No.
+
+
