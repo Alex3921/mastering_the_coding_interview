46dfbcb7ba22b7baf6f860a8621559685c924b61
Alexandru Boncut <boncut.alex10@gmail.com>
2022-04-08 23:58:59 -0500

    Add logic

--- hard/trappingRainwater.js
+++ hard/trappingRainwater.js
@@ -63,5 +63,22 @@ const trappingRainwater = function(height) {
     let rightMax = 0;
     let total = 0;
 
-
+    while(left < right) {
+        if(height[left] <= height[right]) {
+            if(height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                total += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if(height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                total += rightMax - height[right];
+            };
+            right--;
+        }
+        return total;
+    }
 }
